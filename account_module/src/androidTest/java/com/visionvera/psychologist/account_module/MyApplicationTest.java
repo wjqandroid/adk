cb6bb2eb426222243d5dc2203f5ac4b1a0a3bac5
weijiaqi <wei.jiaqi@aliyun.com>
2022-04-23 10:44:44 +0800

    1

---
+++ account_module/src/androidTest/java/com/visionvera/psychologist/account_module/MyApplicationTest.java
@@ -0,0 +1,14 @@
+package com.visionvera.psychologist.account_module;
+
+import junit.framework.TestCase;
+
+/**
+ * @Classname:MyApplicationTest
+ * @author:haohuizhao
+ * @Date:2022/4/22 11:11
+ * @Version：v1.0
+ * @describe：
+ */
+public class MyApplicationTest extends TestCase {
+
+}
